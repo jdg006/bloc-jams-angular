c5821bf36a6e2efaa843cd4e7ff3ae215f4fbd3c
jdg006 <jdg006@lvc.edu>
2017-06-27 13:41:07 -0400

    assignment 7 complete

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,12 +1,31 @@
 (function() {
      function SongPlayer() {
+       /**
+       * @desc songPlayer object
+       * @type {Object}
+       */
           var SongPlayer = {};
-          var currentSong = null;
+
           /**
-           * @desc Buzz object audio file
-           * @type {Object}
-           */
-     var currentBuzzObject = null;
+          * @desc currentSong boolean
+          * @type {boolean}
+          */
+          var currentSong = null;
+        /**
+         * @desc Buzz object audio file
+         * @type {Object}
+         */
+         var currentBuzzObject = null;
+     /**
+      * @function playSong
+      * @desc plays currentBuzzObject and sets song.playing to "true"
+      * @param {Object} song
+      */
+     var playSong = function(song){
+       currentBuzzObject.play();
+       song.playing = true;
+     };
+
      /**
       * @function setSong
       * @desc Stops currently playing song and loads new audio file as currentBuzzObject
@@ -26,21 +45,32 @@
          currentSong = song;
       };
 
+      /**
+       * @function SongPlayer.play
+       * @desc if the song selected is not the currently playing song then setSong() and playSong() are called.
+        If the song selected is the currently playing song then the currentBuzzObject is paused and song.playing
+        is set to false.
+       * @param {Object} song
+       */
           SongPlayer.play = function(song) {
             if (currentSong !== song) {
              setSong(song);
-             currentBuzzObject.play();
-             song.playing = true;
+             playSong(song);
            }
             else if (currentSong === song) {
            if (currentBuzzObject.isPaused()) {
-               currentBuzzObject.play();
+               playSong(song);
            }
        }
      };
+      /**
+     * @function SongPlayer.pause
+     * @desc pauses currentBuzzObject and sets song.playing to false
+     * @param {Object} song
+     */
      SongPlayer.pause = function(song) {
-     currentBuzzObject.pause();
-     song.playing = false;
+       currentBuzzObject.pause();
+       song.playing = false;
  };
           return SongPlayer;
      }
