7280c536c1d13738b8809dd0170d8102360f6490
jdg006 <jdg006@lvc.edu>
2017-06-28 18:03:35 -0400

    fixed newValue

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -67,9 +67,9 @@
      });
  };
 
- var notifyOnChange = function(newValue) {
+ var notifyOnChange = function(a) {
       if (typeof scope.onChange === 'function') {
-          scope.onChange({value: newValue});
+          scope.onChange({value: a});
       }
   };
           }
